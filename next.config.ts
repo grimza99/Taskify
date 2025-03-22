583c34c71dbf6ba8b5deb49cf6c9fcc16d74bd65
nerte <grimza@nerteui-MacBookPro.local>
2025-03-23 00:25:07 +0900

    [build] postcss.config &postcssLoaderOptions removed in  next.config.json

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  postcssLoaderOptions: {
-    postcssOptions: {
-      config: "./postcss.config.js",
-    },
-  },
   images: {
     domains: ["example.com"],
   },
--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   plugins: {
+    "postcss-import": {},
+    tailwindcss: {},
+    autoprefixer: {},
     "@tailwindcss/postcss": {},
   },
 };
